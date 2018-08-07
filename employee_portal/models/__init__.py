e9f4391c570631fe317791737db59f65bc7678c6
Riley Mathews <rileymathews80@gmail.com>
2018-08-07 15:11:27 -0500

    added model to init file

--- employee_portal/models/__init__.py
+++ employee_portal/models/__init__.py
@@ -1,3 +1,4 @@
 from .training_programs_model import Training_Programs_Model
 from .departments_model import Departments
 from .employee_model import Employee_Model
+from .employee_trainings_model import Employee_Trainings_Model

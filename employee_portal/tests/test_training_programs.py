ab36ce024180c57081065b824d9d13f0da2bb198
Riley Mathews <rileymathews80@gmail.com>
2018-08-08 14:20:41 -0500

    added tests for the rest of training program

--- employee_portal/tests/test_training_programs.py
+++ employee_portal/tests/test_training_programs.py
@@ -15,6 +15,7 @@ class Test_Training_Programs(TestCase):
     """
     
     def test_list_training_programs(self):
+        """ method to test training program can be created """
         new_training_program = Training_Programs_Model.objects.create(
             name='test training',
             description='I hope this works!',
@@ -26,6 +27,23 @@ class Test_Training_Programs(TestCase):
         response = self.client.get(reverse('employee_portal:training'))
 
         self.assertEqual(len(response.context['training_list']), 1)
+
+        self.assertIn(new_training_program.name.encode(), response.content)
+
+    def test_training_programs_add(self):
+        """ method to test form for adding a training program """
+
+        response = self.client.get(reverse('employee_portal:add_training'))
+
+        self.assertIn('<input type="text" name="description" maxlength="200" required id="id_description">'.encode(), response.content)
+
+    def test_post_artist(self):
+        """ method to test that we can post an artst to the form """
+
+        response = self.client.post(reverse('employee_portal:add_training'), {'name': 'hello', 'description': 'this is a test', 'start_date': '2019-12-12', 'end_date': '2019-12-12', 'max_attendees': 4})
+
+        # Getting 302 back because we have a success url and the view is redirecting under the covers?
+        self.assertEqual(response.status_code, 302)
 
     
     
--- employee_portal/urls.py
+++ employee_portal/urls.py
@@ -6,5 +6,5 @@ app_name = 'employee_portal'
 urlpatterns = [
     path('', views.Index_View.as_view(), name='index'),
     path('training_programs/', views.Training_Program_List_View.as_view(), name='training'),
-    path('training_programs/add', views.Training_Program_Add_View.as_view(), name='add training')
+    path('training_programs/add', views.Training_Program_Add_View.as_view(), name='add_training')
 ]

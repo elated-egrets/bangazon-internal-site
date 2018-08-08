a2392a3c3bbc47eba3e1bd23e6e2fb040b548169
pzc <paul.zimmerman.clayton@gmail.com>
2018-08-08 15:57:12 -0500

    connected to index now

--- employee_portal/urls.py
+++ employee_portal/urls.py
@@ -6,7 +6,7 @@ from . import views
 
 app_name = 'employee_portal'
 urlpatterns = [
-    path('department/', views.DepartmentList.as_view(), name='department_list'),
+    path('departments/', views.DepartmentList.as_view(), name='department_list'),
     # path('department/<int:pk>', views.DepartmentDetail.as_view(), name='department_detail'),
     # path('department/add', views.department_add, name='department_add'),
     # path('department/<int:pk>/edit', views.department_edit, name='department_edit'),

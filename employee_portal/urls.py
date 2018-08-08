56c84a2ad6d26b0be23778ab9cd35f3fb9ac9c50
Riley Mathews <rileymathews80@gmail.com>
2018-08-08 11:35:30 -0500

    added form for adding training program

--- employee_portal/urls.py
+++ employee_portal/urls.py
@@ -5,5 +5,6 @@ from . import views
 app_name = 'employee_portal'
 urlpatterns = [
     path('', views.Index_View.as_view(), name='index'),
-    path('training_programs/', views.Training_Program_List_View.as_view(), name='training')
+    path('training_programs/', views.Training_Program_List_View.as_view(), name='training'),
+    path('training_programs/add', views.Training_Program_Add_View.as_view(), name='add training')
 ]

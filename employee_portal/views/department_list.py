ee71e84cd871deaa1d7036de778c4acadb096048
pzc <paul.zimmerman.clayton@gmail.com>
2018-08-08 10:44:08 -0500

    added docstrings for model and list

--- employee_portal/views/department_list.py
+++ employee_portal/views/department_list.py
@@ -1,12 +1,33 @@
 from django.views.generic import ListView
 from employee_portal.models import Departments
 
+"""
+author: paul zimmerman-clayton
+
+class view for Department List view
+"""
+
+
 class DepartmentList(ListView):
+  """
+  summary: Class for DepartmentList view
+  
+  specifies Departments model for use in the department list view
+  names context object name as 'department_list'
+  specifies template name as 'employee_portal/department_list.html'
+  """
+
+
   model = Departments
-  # Django defaults to referencing the data in the template as 'object_list'. Here is how we can rename it what we want
   context_object_name = 'department_list'
   template_name = 'employee_portal/department_list.html'
 
-  def department_list(self):  # NOTE that it's the method name that becomes the property on 'view'
+  def department_list(self):
+    """[summary]
+    
+    Returns:
+      [type] -- [description]
+    """
+
     departments = Departments.objects.all()
     return departments

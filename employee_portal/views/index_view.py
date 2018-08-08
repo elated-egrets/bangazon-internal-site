c249f888694d26b7d47b6343e4dba3b4fb524251
Ryoung27 <RRYoung89@gmail.com>
2018-08-08 10:00:32 -0500

    doc clean up

--- employee_portal/views/index_view.py
+++ employee_portal/views/index_view.py
@@ -3,9 +3,10 @@ from django.views.generic import TemplateView
 
 
 class Index_View(TemplateView):
-  template_name = 'employee_portal/index.html'
-  # def location(self):
-  #   return 'home'
   """
       The purpose of this class is to create the index page for the site. This just allows us to have a landing page where we can link to our other views.
   """
+  template_name = 'employee_portal/index.html'
+  # def location(self):
+  #   return 'home'
+

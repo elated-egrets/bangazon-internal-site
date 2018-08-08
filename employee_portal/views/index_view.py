d14662cc31c35d84653270add36a1ca7498b50a8
Ryoung27 <RRYoung89@gmail.com>
2018-08-08 09:41:42 -0500

    doc strings added

--- employee_portal/views/index_view.py
+++ employee_portal/views/index_view.py
@@ -6,3 +6,6 @@ class Index_View(TemplateView):
   template_name = 'employee_portal/index.html'
   # def location(self):
   #   return 'home'
+  """
+      The purpose of this class is to create the index page for the site. This just allows us to have a landing page where we can link to our other views.
+  """

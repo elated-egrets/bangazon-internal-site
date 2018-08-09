ba5336b9a9f8feb63a4ad9aa301028e69b52bb8c
Ryoung27 <RRYoung89@gmail.com>
2018-08-09 11:21:13 -0500

    hr details and test

--- employee_portal/tests/test_employee_detail.py
+++ employee_portal/tests/test_employee_detail.py
@@ -18,7 +18,7 @@ class Test_Employee_Details(TestCase):
     """
     def test_list_employee_details(self):
         """method to test employee detail can be created"""
-        new_employee = Employee_Details.objects.create(
+        new_employee = Employee_Model.objects.create(
             first_name="Richard",
             last_name="What",
             department_id=test_department,
@@ -27,31 +27,13 @@ class Test_Employee_Details(TestCase):
             is_supervisor=False
         )
 
+        response = self.client.get(reverse('employee_portal:employee_detail', kwargs={"pk":1}))
+        self.assertEqual(response.status_code, 200)
 
         # Is this stuff in the content of the body?
         # .encode converts from unicode to utf-8
         # example:
         # If the string is: pythön!
         # The encoded version is: b'pyth\xc3\xb6n!'
-        self.assertIn(new_employee.first_name.encode(), response.content)
-        self.assertIn(new_employee.last_name.encode(), response.content)
-
-    def test_details_post(self):
-
-        response = self.client.post(reverse(), {
-
-        })
-
-        self.assertEqual(response.status_code, 302)
-
-
-
-
-
-
-    def test_post_artist(self):
-
-      response = self.client.post(reverse('history:artist_form'), {'name': 'Bill Board', 'birth_date': '10/31/67', 'biggest_hit': "So Blue Fer You"})
-
-      # Getting 302 back because we have a success url and the view is redirecting under the covers?
-      self.assertEqual(response.status_code, 302)
+        # self.assertIn(new_employee.first_name.encode(), response.content)
+        # self.assertIn(new_employee.last_name.encode(), response.content)
--- employee_portal/urls.py
+++ employee_portal/urls.py
@@ -1,17 +1,11 @@
 from django.urls import path
-from django.views.generic import ListView, DetailView
-
 from . import views
 
 
 app_name = 'employee_portal'
 urlpatterns = [
-    path('departments/', views.DepartmentList.as_view(), name='department_list'),
-    # path('department/<int:pk>', views.DepartmentDetail.as_view(), name='department_detail'),
-    # path('department/add', views.department_add, name='department_add'),
-    # path('department/<int:pk>/edit', views.department_edit, name='department_edit'),
     path('', views.Index_View.as_view(), name='index'),
     path('training_programs/', views.Training_Program_List_View.as_view(), name='training'),
-    path('training_programs/add', views.Training_Program_Add_View.as_view(), name='add_training'),
-    path('employees/<int:pk>/', views.Employee_Detail_View.as_view(), name='employee_detail')
+    path('training_programs/add', views.Training_Program_Add_View.as_view(), name='add_training')
+    path('employee/<int:pk>', views.EmployeeDetail.as_view(), name="employee_detail")
 ]

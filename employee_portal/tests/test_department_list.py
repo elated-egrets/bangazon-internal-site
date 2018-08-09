5aa1b7fab1d31cb5d07bed39e327e9a4611fa2f4
pzc <paul.zimmerman.clayton@gmail.com>
2018-08-09 13:55:05 -0500

    test done for department list

---
+++ employee_portal/tests/test_department_list.py
@@ -0,0 +1,35 @@
+import unittest
+from django.test import TestCase
+from django.urls import reverse
+from employee_portal.models import Departments
+
+
+"""
+    module: test for department model
+    author: paul zimmerman-clayton
+    purpose: testing the department model
+"""
+
+class Test_Departments(TestCase):
+    """
+    class to test the departments model
+    """
+    
+    def test_departments_context(self):
+        """method to test department can be created
+        """
+        new_department = Departments.objects.create(
+            name='test department'
+        )
+
+        response = self.client.get(reverse('employee_portal:department_list'))
+        
+        """tests to make sure there is only one item created
+        """
+
+        self.assertEqual(len(response.context['department_list']), 1)
+
+        """tests to make sure the name is what is expected
+        """
+
+        self.assertIn(new_department.name.encode(), response.content)

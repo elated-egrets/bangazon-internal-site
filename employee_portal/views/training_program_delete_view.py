8d365483e4a3f6db06bac85d5efdec82c3d566fc
Riley Mathews <rileymathews80@gmail.com>
2018-08-09 13:35:25 -0500

    got reverse url lookup working with reverse lazy

--- employee_portal/views/training_program_delete_view.py
+++ employee_portal/views/training_program_delete_view.py
@@ -1,5 +1,5 @@
 from django.views.generic import DeleteView
-from django.urls import reverse
+from django.urls import reverse_lazy
 from employee_portal.models import Training_Programs_Model
 
 """  
@@ -12,6 +12,5 @@ class Training_Program_Delete_View(DeleteView):
     """ class to create the view that will be used for deleting a training program """
 
     model = Training_Programs_Model
-    success_url = '/training_programs/'
-    # success_url = reverse('employee_portal:training')
+    success_url = reverse_lazy('employee_portal:training')
     template_name = 'employee_portal/training_program_delete.html'

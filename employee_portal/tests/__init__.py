f8df351f94752f7aacec9034c934c68ddff044ab
Ryoung27 <RRYoung89@gmail.com>
2018-08-09 14:04:44 -0500

    testing

--- employee_portal/tests/__init__.py
+++ employee_portal/tests/__init__.py
@@ -1,7 +1,4 @@
 from .test_training_programs import Test_Training_Programs
-<<<<<<< HEAD
 from .test_employee_model import Test_Employee_Models
 from .test_employee_detail import Test_Employee_Details
-=======
 from .test_employee_list import Test_Employee_Models
->>>>>>> master

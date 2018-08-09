6f26ec9661e859e9020702d489fe9d2423035f63
pzc <paul.zimmerman.clayton@gmail.com>
2018-08-09 09:38:15 -0500

    fixing conflicts

--- employee_portal/management/commands/faker_factory.py
+++ employee_portal/management/commands/faker_factory.py
@@ -2,13 +2,7 @@ from django.core.management.base import BaseCommand
 from django_seed import Seed
 seeder = Seed.seeder()
 import random
-<<<<<<< HEAD
-from employee_portal.models import Training_Programs_Model, Training_Program_Events_Model, Departments, Employee_Model
-=======
-
-from employee_portal.models import Training_Programs_Model, Training_Program_Events_Model, Departments
-
->>>>>>> 815899c4b81e79f8dc8f8d82ebb1f4079dc43717
+from employee_portal.models import Training_Programs_Model, Departments, Employee_Model
 
 
 class Command(BaseCommand):
@@ -20,14 +14,9 @@ class Command(BaseCommand):
         # the number argument is the total num of rows you want created
 
         seeder.add_entity(Training_Programs_Model, 12)
-        seeder.add_entity(Training_Program_Events_Model, 12)
         # seeder.add_entity(models.employees, 12)
         seeder.add_entity(Departments, 12)
         # seeder.add_entity(models.employee_trainings, 12)
         seeder.add_entity(Employee_Model, 12)
-<<<<<<< HEAD
-=======
-
->>>>>>> 815899c4b81e79f8dc8f8d82ebb1f4079dc43717
 
         inserted_pks = seeder.execute()
--- employee_portal/views/__init__.py
+++ employee_portal/views/__init__.py
@@ -2,7 +2,3 @@ from .department_list import DepartmentList
 from .index_view import Index_View
 from .training_program_list_view import Training_Program_List_View
 from .training_program_add_view import Training_Program_Add_View
-<<<<<<< HEAD
-=======
-
->>>>>>> 815899c4b81e79f8dc8f8d82ebb1f4079dc43717
